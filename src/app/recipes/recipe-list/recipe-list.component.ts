4471050511383afd58a33651d0ddbfee24fef690
nkidmano <nobudy55@gmail.com>
2018-08-11 21:40:52 +0700

    finish section 3

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,17 +1,22 @@
-import { Component, OnInit } from "@angular/core";
-import { Recipe } from "../recipe.model";
+import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
-  selector: "app-recipe-list",
-  templateUrl: "./recipe-list.component.html",
-  styleUrls: ["./recipe-list.component.css"]
+  selector: 'app-recipe-list',
+  templateUrl: './recipe-list.component.html',
+  styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
   recipes: Recipe[] = [
     new Recipe(
-      "Pizza",
-      "Frenze pizza with chinese shit",
-      "https://upload.wikimedia.org/wikipedia/commons/a/a3/Eq_it-na_pizza-margherita_sep2005_sml.jpg"
+      'Pizza',
+      'Frenze pizza with chinese shit',
+      'https://upload.wikimedia.org/wikipedia/commons/a/a3/Eq_it-na_pizza-margherita_sep2005_sml.jpg'
+    ),
+    new Recipe(
+      'Meatball',
+      'Meatball with spagetthi',
+      'https://images-gmi-pmc.edge-generalmills.com/05d6e7b7-f677-4845-89d2-7e965c2d9538.jpg'
     )
   ];
 
--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -6,10 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./shopping-edit.component.css']
 })
 export class ShoppingEditComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
